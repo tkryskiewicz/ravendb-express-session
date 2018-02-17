30a964ef1a8c4cf14ee87f2b4c8f5eb904dd57a1
Tomasz Kryskiewicz <tomasz.kryskiewicz@infor.com>
2018-02-17 22:04:56 +0100

    implement set operation

--- src/RavenDbSessionStore.test.ts
+++ src/RavenDbSessionStore.test.ts
@@ -1,9 +1,97 @@
+import { DocumentStore } from "ravendb";
+
 import { RavenDbSessionStore } from "./RavenDbSessionStore";
 
 describe("RavenDbSessionStore", () => {
+  let documentStore: DocumentStore;
+
+  beforeAll(() => {
+    documentStore = new DocumentStore("http://127.0.0.1:8080", "Test");
+
+    documentStore.initialize();
+  });
+
+  const generateSessionId = () => Math.random().toString().substr(2, 9);
+
+  const loadSessionDocument = async (id: string) => {
+    const documentSession = documentStore.openSession();
+
+    const sessionDocument = await documentSession.load(id, { documentType: "Session" });
+
+    return sessionDocument;
+  };
+
   it("should be constructable", () => {
-    const instance = new RavenDbSessionStore();
+    const instance = new RavenDbSessionStore(documentStore);
 
     expect(instance).toBeDefined();
+  });
+
+  describe("set", () => {
+    it("should store session document", (done) => {
+      const instance = new RavenDbSessionStore(documentStore);
+
+      const sessionId = generateSessionId();
+
+      const session: Express.Session = {
+        id: sessionId,
+      } as any;
+
+      instance.set(sessionId, session, () => {
+        (async () => {
+          const sessionDocument = await loadSessionDocument(sessionId);
+
+          expect(sessionDocument).toBeDefined();
+          expect(sessionDocument.id).toBe(sessionId);
+
+          done();
+        })();
+      });
+    });
+
+    it("should store session document with sid parameter", (done) => {
+      const instance = new RavenDbSessionStore(documentStore);
+
+      const sessionId = generateSessionId();
+      const otherSessionId = generateSessionId();
+
+      const session: Express.Session = {
+        id: otherSessionId,
+      } as any;
+
+      instance.set(sessionId, session, () => {
+        (async () => {
+          const sessionDocument = await loadSessionDocument(sessionId);
+          const otherSessionDocument = await loadSessionDocument(otherSessionId);
+
+          expect(sessionDocument).toBeDefined();
+          expect(sessionDocument.id).toBe(sessionId);
+          expect(otherSessionDocument).toBeNull();
+
+          done();
+        })();
+      })
+    });
+
+    it("should store session data", (done) => {
+      const instance = new RavenDbSessionStore(documentStore);
+
+      const sessionId = generateSessionId();
+
+      const session: Express.Session = {
+        id: sessionId,
+        data: "data",
+      } as any;
+
+      instance.set(sessionId, session, () => {
+        (async () => {
+          const sessionDocument = await loadSessionDocument(sessionId);
+
+          expect(sessionDocument.data).toBe("data");
+
+          done();
+        })();
+      });
+    });
   });
 });
--- src/RavenDbSessionStore.ts
+++ src/RavenDbSessionStore.ts
@@ -1,2 +1,33 @@
-export class RavenDbSessionStore {
+import { Store } from "express-session";
+import { DocumentStore } from "ravendb";
+
+export class RavenDbSessionStore extends Store {
+  constructor(private documentStore: DocumentStore) {
+    super();
+  }
+
+  public set = (sid: string, session: Express.Session, callback: (err: any) => void) => {
+    this.setSession(sid, session)
+      .then(() => {
+        callback(undefined);
+      })
+      .catch((error) => {
+        callback(error);
+      });
+  }
+
+  private async setSession(sessionId: string, session: Express.Session) {
+    const documentSession = this.documentStore.openSession();
+
+    const sessionDocument = {
+      ...session,
+      id: sessionId,
+    };
+
+    await documentSession.store(sessionDocument, sessionId, {
+      documentType: "Session",
+    });
+
+    await documentSession.saveChanges();
+  }
 }

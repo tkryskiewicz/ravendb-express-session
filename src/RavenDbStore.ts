0453fd6d845108f4af610af120ae326338586b93
Tomasz Kryskiewicz <tomasz.kryskiewicz@infor.com>
2018-02-22 20:46:20 +0100

    implement a common promise + callback handler

--- src/RavenDbStore.ts
+++ src/RavenDbStore.ts
@@ -5,6 +5,10 @@ export interface SessionDocument {
   data: string;
 }
 
+export interface SessionDataCollection {
+  [sid: string]: Express.SessionData;
+}
+
 export interface RavenDbStoreOptions {
   documentType: string;
 }
@@ -26,105 +30,27 @@ export class RavenDbStore extends Store {
   }
 
   public set = (sid: string, session: Express.Session, callback?: (err: any) => void) => {
-    return this.setSession(sid, session)
-      .then(() => {
-        if (callback) {
-          callback(undefined);
-        }
-      })
-      .catch((error) => {
-        if (callback) {
-          callback(error);
-        }
-
-        throw error;
-      });
+    return this.handlePromise(this.setSession(sid, session), callback);
   }
 
   public get = (sid: string, callback?: (err: any, sessionData: Express.SessionData) => void) => {
-    return this.getSessionData(sid)
-      .then((sessionData) => {
-        if (callback) {
-          callback(undefined, sessionData as any);
-        }
-
-        return sessionData;
-      })
-      .catch((error) => {
-        if (callback) {
-          callback(error, undefined as any);
-        }
-
-        throw error;
-      });
+    return this.handlePromise(this.getSessionData(sid), callback);
   }
 
   public destroy = (sid: string, callback?: (err: any) => void) => {
-    return this.destroySession(sid)
-      .then(() => {
-        if (callback) {
-          callback(undefined);
-        }
-      })
-      .catch((error) => {
-        if (callback) {
-          callback(error);
-        }
-
-        throw error;
-      });
+    return this.handlePromise(this.destroySession(sid), callback);
   }
 
-  public all = (callback?: (err: any, obj: { [sid: string]: Express.SessionData; }) => void) => {
-    return this.getAllSessions()
-      .then((sessions) => {
-        if (callback) {
-          callback(undefined, sessions);
-        }
-
-        return sessions;
-      })
-      .catch((error) => {
-        if (callback) {
-          callback(error, undefined as any);
-        }
-
-        throw error;
-      });
+  public all = (callback?: (err: any, obj: SessionDataCollection) => void) => {
+    return this.handlePromise(this.getAllSessions(), callback);
   }
 
   public clear = (callback?: (err: any) => void) => {
-    return this.clearSessions()
-      .then(() => {
-        if (callback) {
-          callback(undefined);
-        }
-      })
-      .catch((error) => {
-        if (callback) {
-          callback(error);
-        }
-
-        throw error;
-      });
+    return this.handlePromise(this.clearSessions(), callback);
   }
 
   public length = (callback?: (err: any, length: number) => void) => {
-    return this.getCount()
-      .then((count) => {
-        if (callback) {
-          callback(undefined, count);
-        }
-
-        return count;
-      })
-      .catch((error) => {
-        if (callback) {
-          callback(error, undefined as any);
-        }
-
-        throw error;
-      });
+    return this.handlePromise(this.getCount(), callback);
   }
 
   private async setSession(sessionId: string, session: Express.Session) {
@@ -139,14 +65,14 @@ export class RavenDbStore extends Store {
     await documentSession.saveChanges();
   }
 
-  private async getSessionData(sessionId: string): Promise<Express.SessionData | undefined> {
+  private async getSessionData(sessionId: string): Promise<Express.SessionData> {
     const documentSession = this.documentStore.openSession();
 
     const sessionDocument = await documentSession.load<SessionDocument>(sessionId, {
       documentType: this.options.documentType,
     });
 
-    return sessionDocument ? this.deserializeSession(sessionDocument) : undefined;
+    return sessionDocument ? this.deserializeSession(sessionDocument) : undefined as any;
   }
 
   private async destroySession(sessionId: string) {
@@ -172,7 +98,7 @@ export class RavenDbStore extends Store {
 
     const sessions = sessionDocuments
       .map((document): Express.SessionData => this.deserializeSession(document))
-      .reduce((p: { [sessionId: string]: Express.SessionData }, c) => {
+      .reduce((p: SessionDataCollection, c) => {
         p[c.id] = c;
 
         return p;
@@ -217,5 +143,23 @@ export class RavenDbStore extends Store {
 
   private deserializeSession(document: SessionDocument): Express.SessionData {
     return JSON.parse(document.data);
+  }
+
+  private handlePromise<T>(promise: Promise<T>, callback?: (err: any, result: T) => void) {
+    return promise
+      .then((result) => {
+        if (callback) {
+          callback(undefined, result);
+        }
+
+        return result;
+      })
+      .catch((error) => {
+        if (callback) {
+          callback(error, undefined as any);
+        }
+
+        throw error;
+      });
   }
 }
